eb3b721441989f0e7e2fc811586b7c04580b5e47
joshaw807 <joshaw807@gmail.com>
2025-05-09 15:54:02 -0400

    added clickable grid with a reset button

--- javascript.js
+++ javascript.js
@@ -1,9 +1,21 @@
-const gridBody = document.querySelector("div.gridContainer");
 
-//creates the grid
-for(let i =0; i< 9; i++){
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("gridItem");
-    gridItem.textContent = i;
-    gridBody.appendChild(gridItem);
-}
+    const gridBody = document.querySelector("div.gridContainer");
+
+    //creates the grid
+    for(let i =0; i< 9; i++){
+        const gridItem = document.createElement("button");
+        gridItem.classList.add("gridItem");
+     //   gridItem.textContent = i;
+        gridBody.appendChild(gridItem);
+    }
+
+
+
+const gridItems = document.querySelectorAll(".gridItem");
+gridItems.forEach((item) =>{
+    item.addEventListener("click", (e) => {
+        const clickedItem = e.target;
+        clickedItem.classList.toggle("clicked");
+        console.log(clickedItem);
+    });
+});
