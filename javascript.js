1894b23992bbdb432a5f9c4faa42602017f230a4
joshaw807 <joshaw807@gmail.com>
2025-05-09 21:11:50 -0400

    Created a stable working tic tac toe that has a reset button

--- javascript.js
+++ javascript.js
@@ -1,21 +1,77 @@
 
-    const gridBody = document.querySelector("div.gridContainer");
+const gridBody = document.querySelector("div.gridContainer");
 
-    //creates the grid
-    for(let i =0; i< 9; i++){
-        const gridItem = document.createElement("button");
-        gridItem.classList.add("gridItem");
-     //   gridItem.textContent = i;
-        gridBody.appendChild(gridItem);
-    }
+//creates the grid
+for(let i =0; i< 9; i++){
+    const gridItem = document.createElement("button");
+    gridItem.classList.add("gridItem");
+//    gridItem.textContent = i;
+    gridBody.appendChild(gridItem);
+}
 
+let playerO = true; //lets player start
+
+const winPattern =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8]
+]
 
 
 const gridItems = document.querySelectorAll(".gridItem");
 gridItems.forEach((item) =>{
-    item.addEventListener("click", (e) => {
-        const clickedItem = e.target;
-        clickedItem.classList.toggle("clicked");
-        console.log(clickedItem);
+    item.addEventListener("click", () => {
+        if(playerO){
+            item.innerHTML = "O"
+            playerO = false;
+            item.disabled = true;
+            checkWinner();
+        }else{
+            item.innerHTML = "X"
+            playerO = true;
+            item.disabled = true;
+            checkWinner();
+        }
     });
 });
+
+
+function checkWinner(){
+    for(let pattern of winPattern){
+        const [a,b,c] = pattern;
+
+        if( gridItems[a].innerHTML !== "" && 
+            gridItems[a].innerHTML === gridItems[b].innerHTML &&
+            gridItems[b].innerHTML === gridItems[c].innerHTML
+        ){
+            alert(`Player ${gridItems[a].textContent} wins!`);
+            disableBox();
+            return;
+        }
+    }
+}
+
+function enableGrid(){
+    gridItems.forEach(item =>{
+        item.disabled = false;
+        item.innerHTML = "";
+    })
+}
+
+function disableBox(){
+
+    for(let box of gridItems){
+            box.disabled = true;
+            console.log(box);
+    }
+}
+
+function resetGame(){
+    playerO = true;
+    enableGrid();
+}
